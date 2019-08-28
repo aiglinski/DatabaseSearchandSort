212456263df54f4fc1b16b6e2b9286a3e6b14849
Andrew <aiglinski@icloud.com>
2019-08-28 17:04:31 -0500

    user search, profile display, and clear profile on deleted entry functionality complete

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // *Initialize github function
 const github = new Github;
+// *Initialize UI JS
+const ui = new UI;
 
 // *Entry point JS file,  Search input
 const searchUser = document.getElementById('search-user');
 //* search input event listenter
-searchUser.addEventListener('keyup', (e) => {
+searchUser.addEventListener('keyup', e => {
     // get Input Text
     const userText = e.target.value;
 
-    if (userText !== '') {
+    if (userText !== "") {
         //* make HTTP request
-        github.getUser(userText)
-            .then(data => console.log(data))
+        github.getUser(userText).then(data => {
+            if (data.profile.message === 'Not Found') {
+                // *Show Error
+            } else {
+                // *Show profile
+                ui.showProfile(data.profile);
+            }
+        });
     }
-})
+     else {
+        // *Clear profile if search is empty
+        ui.clearProfile();
+      }
+});
